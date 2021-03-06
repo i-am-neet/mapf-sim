246c7ed7d66061f152218a058f5b82a12e114a53
NEET <zxc455233@gmail.com>
2021-03-07 02:08:01 +0800

    Add movebase parameters and launch files for local map
    bringup_all.bash to startup simulator, all robot's tf, and all robot's movebase
    quit_all.bash to shutdown processes that start from bringup_all.bash

---
+++ robot_stage/scripts/bringup_all.bash
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+echo "LAUNCHING turtles..."
+screen -dmS turtles_sim roslaunch robot_stage turtles.launch
+sleep 3
+for i in {0..3};
+do
+  echo "LAUNCHING robot_$i's tf transformer..."
+  screen -dmS turtles${i}_tf roslaunch robot_stage tf2_broadcaster.launch ns:=$i robot:=/robot_$i
+done
+sleep 3
+for i in {0..3};
+do
+  echo "LAUNCHING robot_$i's movebase..."
+  screen -dmS turtles${i}_movebase roslaunch robot_stage turtle_movebase.launch ns:=$i robot:=robot_$i
+done
+echo "done. view with screen -ls"
--- robot_stage/scripts/quit_all.bash
+++ robot_stage/scripts/quit_all.bash
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
+screen -X -S turtles_sim quit
 for i in {0..3};
 do
   echo "Closeing robot_$i's tf transformer..."
   screen -X -S turtles${i}_tf quit
+  echo "Closeing robot_$i's movebase..."
+  screen -X -S turtles${i}_movebase quit
 done
 echo "done. view with screen -ls"
