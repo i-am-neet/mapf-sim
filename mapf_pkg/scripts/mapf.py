f550ea0b1132c96764d7d531fff91748890e6b96
NEET <zxc455233@gmail.com>
2021-03-20 22:10:42 +0800

    Fix tensor of neighbors' goal

--- mapf_pkg/scripts/mapf.py
+++ mapf_pkg/scripts/mapf.py
@@ -2,6 +2,7 @@
 import sys
 from signal import signal, SIGINT
 from sys import exit
+import math
 import copy
 import numpy as np
 import rospy
@@ -21,16 +22,16 @@ map_resolution = 0.01
 ## Tmp
 goals = [(2, 2), (-1.5, 1.5), (-1.5, -1.5), (2, -2)]
 
-def draw_robot(map, map_width, map_height, rx, ry):
+def draw_robot(map, map_width, map_height, rx, ry, stick_on_bound=False):
     _r = robot_radius/map_resolution
     rx = rx + map_width/2
     ry = map_height/2 - ry
-    x_start = int(abs(rx-_r)) if int(abs(rx-_r))>=0 else 0
-    x_end = int(abs(rx+_r)) if int(abs(rx+_r))<map_width else map_width
-    y_start = int(abs(ry-_r)) if int(abs(ry-_r))>0 else 0
-    y_end = int(abs(ry+_r)) if int(abs(ry+_r))<map_height else map_height
 
-    # _map = torch.zeros(map_size)
+    x_start = int(rx-_r) if int(rx-_r)>=0 else 0
+    x_end = int(rx+_r) if int(rx+_r)<map_width else map_width
+    y_start = int(ry-_r) if int(ry-_r)>0 else 0
+    y_end = int(ry+_r) if int(ry+_r)<map_height else map_height
+
     _map = copy.deepcopy(map)
 
     for x in range(x_start, x_end):
@@ -42,8 +43,24 @@ def draw_robot(map, map_width, map_height, rx, ry):
 def draw_goal(map, map_width, map_height, gx, gy):
     return draw_robot(map, map_width, map_height, gx, gy)
 
-def draw_neighbors_goal(map, map_width, map_height, ngx, ngy):
-    return draw_robot(map, map_width, map_height, ngx, ngy)
+def draw_neighbors_goal(map, map_width, map_height, ngx, ngy, myx, myy):
+    _r = robot_radius/map_resolution
+    dx = ngx - myx
+    dy = ngy - myy
+    m = dy/dx if dx != 0 else math.inf
+    if abs(dx) <= map_width/2-_r and abs(dy) <= map_height/2-_r:     # in the FOV
+        return draw_robot(map, map_width, map_height, dx, dy, True)
+    elif m == math.inf:                                              # goal on vertical
+        vy = np.sign(dy)*map_height/2
+        return draw_robot(map, map_width, map_height, 0, vy, True)
+    elif abs(m) <= 1:                                                # slope(m) < 1
+        vx = np.sign(dx)*map_width/2                                 # draw on x-axis edge of fov
+        vy = m*vx                                                    # y=mx+b. bias(b) is 0, because (0, 0) is self
+        return draw_robot(map, map_width, map_height, vx, vy, True)
+    elif abs(m) > 1:                                                 # slope(m) < 1
+        vy = np.sign(dy)*map_height/2                                # draw on y-axis edge of fov
+        vx = vy/m
+        return draw_robot(map, map_width, map_height, vx, vy, True)
 
 """
     callback value:
@@ -69,6 +86,7 @@ def callback(*data):
 
     agx = goals[int(current_robot_num)-1][0] / map_resolution
     agy = goals[int(current_robot_num)-1][1] / map_resolution
+
     my_goal_map = draw_goal(my_goal_map, map_width, map_height, agx - my_x, agy - my_y)
 
     for i, e in enumerate(data):
@@ -76,40 +94,14 @@ def callback(*data):
             _rx = e.pose.pose.position.x / map_resolution
             _ry = e.pose.pose.position.y / map_resolution
             if abs(_rx - my_x) <= map_width/2 and abs(_ry - my_y) <= map_height/2:
+
                 agents_map = draw_robot(agents_map, map_width, map_height, _rx - my_x, _ry - my_y)
 
                 ## Neighbors
                 if i != int(current_robot_num):
-                    _nx = e.pose.pose.position.x / map_resolution
-                    _ny = e.pose.pose.position.y / map_resolution
                     _ngx = goals[i-1][0] / map_resolution
                     _ngy = goals[i-1][1] / map_resolution
-
-                    v_ng_m_x = _ngx - my_x
-                    v_ng_m_y = _ngy - my_y
-                    slope = v_ng_m_y/v_ng_m_x
-                    b = _ngy - slope*_ngx
-                    if abs(slope) < 1:
-                        if _ngx < my_x - map_width/2:#L BUGGG
-                            _x = my_x-map_width/2 + (robot_radius/map_resolution)/2
-                            _y = slope*_x+b
-                        elif _ngx > my_x + map_width/2:#R OK
-                            _x = my_x+map_width/2
-                            _y = slope*_x+b
-                        else:
-                            _x = _ngx
-                            _y = _ngy
-                    else:
-                        if _ngy < my_y - map_height/2:#D OK
-                            _y = my_y-map_height/2
-                            _x = (_y-b)/slope
-                        elif _ngy > my_y + map_height/2:#U BUGGGGGGGGG
-                            _y = my_y+map_height/2 + (robot_radius/map_resolution)/2
-                            _x = (_y-b)/slope
-                        else:
-                            _x = _ngx
-                            _y = _ngy
-                    neighbors_goal_map = draw_neighbors_goal(neighbors_goal_map, map_width, map_height, _x - my_x, _y - my_y)
+                    neighbors_goal_map = draw_neighbors_goal(neighbors_goal_map, map_width, map_height, _ngx, _ngy, my_x, my_y)
 
     cv2.imshow("local_map", local_map.reshape(map_height, map_width).cpu().numpy().astype('uint8'))
     cv2.imshow("agents_map", agents_map.reshape(map_height, map_width).cpu().numpy().astype('uint8'))
