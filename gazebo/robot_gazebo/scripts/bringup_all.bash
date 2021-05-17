d21d41553c4e36030dccbf6043e5e31aaa5e2da9
NEET <zxc455233@gmail.com>
2021-05-18 02:18:40 +0800

    Spawn robots by bringup_all.bash with cfg file which define all robots initial pose

--- gazebo/robot_gazebo/scripts/bringup_all.bash
+++ gazebo/robot_gazebo/scripts/bringup_all.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-TOTAL_ROBOT_NUM=3
+# TOTAL_ROBOT_NUM=3
 
 #echo "LAUNCHING turtles..."
 #screen -dmS turtles_sim roslaunch robot_stage turtles.launch
@@ -20,11 +20,21 @@ TOTAL_ROBOT_NUM=3
 # done
 # echo "done. view with screen -ls"
 
-for i in $(seq 0 $TOTAL_ROBOT_NUM);
+# for i in $(seq 0 $TOTAL_ROBOT_NUM);
+# do
+#   echo "SPAWNING robot$i..."
+#   # screen -dmS turtles${i}_movebase roslaunch robot_stage turtle_movebase.launch ns:=$i robot:=R1
+#   screen -dmS spawn_robot${i} roslaunch robot_gazebo spawn-robot.launch id:=$i
+#   # roslaunch robot_stage turtle_movebase.launch ns:=stage/R1 robot:=R1
+# done
+# echo "done. view with screen -ls"
+
+let i=0
+while IFS=' ' read -r -a poses_arr;
 do
-  echo "SPAWNING robot$i..."
-  # screen -dmS turtles${i}_movebase roslaunch robot_stage turtle_movebase.launch ns:=$i robot:=R1
-  screen -dmS spawn_robot${i} roslaunch robot_gazebo spawn-robot.launch id:=$i
-  # roslaunch robot_stage turtle_movebase.launch ns:=stage/R1 robot:=R1
-done
-echo "done. view with screen -ls"
+  echo "SPAWNING robot$i, pose at: (${poses_arr[0]}, ${poses_arr[1]}, ${poses_arr[2]})..."
+  screen -dmS spawn_robot${i} roslaunch robot_gazebo spawn-robot.launch id:=$i \
+              x_pos:=${poses_arr[0]} y_pos:=${poses_arr[1]} yaw_pos:=${poses_arr[2]}
+  sleep 0.5
+  ((i++))
+done < $(rosls robot_gazebo/config/init_poses.cfg)
