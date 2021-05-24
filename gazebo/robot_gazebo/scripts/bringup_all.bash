672d10a8eff1383af10bd83f272def60686342e0
NEET <zxc455233@gmail.com>
2021-05-25 05:39:50 +0800

    Fix planar_move publish topics' name & beautify script

--- gazebo/robot_gazebo/scripts/bringup_all.bash
+++ gazebo/robot_gazebo/scripts/bringup_all.bash
@@ -1,34 +1,5 @@
 #!/usr/bin/env bash
 
-# TOTAL_ROBOT_NUM=3
-
-#echo "LAUNCHING turtles..."
-#screen -dmS turtles_sim roslaunch robot_stage turtles.launch
-#sleep 3
-# for i in $(seq 0 $TOTAL_ROBOT_NUM);
-# do
-#   echo "LAUNCHING robot_$i's tf transformer..."
-#   screen -dmS turtles${i}_tf roslaunch robot_stage tf2_broadcaster.launch ns:=$i robot:=robot_$i
-# done
-# sleep 3
-# for i in $(seq 0 $TOTAL_ROBOT_NUM);
-# do
-#   echo "LAUNCHING robot_$i's movebase..."
-#   # screen -dmS turtles${i}_movebase roslaunch robot_stage turtle_movebase.launch ns:=$i robot:=R1
-#   screen -dmS turtles${i}_movebase roslaunch robot_stage turtle_movebase.launch robot:=robot_$i
-#   # roslaunch robot_stage turtle_movebase.launch ns:=stage/R1 robot:=R1
-# done
-# echo "done. view with screen -ls"
-
-# for i in $(seq 0 $TOTAL_ROBOT_NUM);
-# do
-#   echo "SPAWNING robot$i..."
-#   # screen -dmS turtles${i}_movebase roslaunch robot_stage turtle_movebase.launch ns:=$i robot:=R1
-#   screen -dmS spawn_robot${i} roslaunch robot_gazebo spawn-robot.launch id:=$i
-#   # roslaunch robot_stage turtle_movebase.launch ns:=stage/R1 robot:=R1
-# done
-# echo "done. view with screen -ls"
-
 let i=0
 while IFS=' ' read -r -a poses_arr;
 do
