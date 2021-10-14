4e46b4daa28047aad71305732e7f7c9e9d6df2a4
NEET <zxc455233@gmail.com>
2021-10-15 02:35:14 +0800

    Remove obstacles_layer and remove inflation_layer from local_costmap
    因為在反覆重置機器人位置時，可能因為obstacles_layer的lidar影響，
    導致global_costmap產生不必要的障礙物，進而影響到路徑規劃。
    此外從local_costmap移除inflation_layer是為了提供神經網路乾淨的輸入

--- gazebo/robot_gazebo/scripts/axclient.py
+++ gazebo/robot_gazebo/scripts/axclient.py
@@ -13,17 +13,17 @@ def movebase_client():
     goal = MoveBaseGoal()
     goal.target_pose.header.frame_id = "map"
     goal.target_pose.header.stamp = rospy.Time.now()
-    goal.target_pose.pose.position.x = 2.0
-    goal.target_pose.pose.position.y = 2.0
+    goal.target_pose.pose.position.x = -2.0
+    goal.target_pose.pose.position.y = -2.0
     goal.target_pose.pose.orientation.w = 1.0
 
     client.send_goal(goal)
-    wait = client.wait_for_result()
-    if not wait:
-        rospy.logerr("Action server not available!")
-        rospy.signal_shutdown("Action server not available!")
-    else:
-        return client.get_result()
+    #wait = client.wait_for_result()
+    #if not wait:
+    #    rospy.logerr("Action server not available!")
+    #    rospy.signal_shutdown("Action server not available!")
+    #else:
+    #    return client.get_result()
 
 if __name__ == '__main__':
     try:
