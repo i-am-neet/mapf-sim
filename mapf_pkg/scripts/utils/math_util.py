9bbba4a1260b7a4439185f37a8c1fb3ccc43f412
NEET <zxc455233@gmail.com>
2021-11-04 01:35:15 +0800

    Add angle calculator between vectors

--- mapf_pkg/scripts/utils/math_util.py
+++ mapf_pkg/scripts/utils/math_util.py
@@ -1,5 +1,24 @@
 import math
+import numpy as np
 
 # dist acheived in Python 3.8 officially
 def dist(p, q):
     return math.sqrt(sum((px - qx)**2 for px, qx in zip(p, q)))
+
+def unit_vector(vector):
+    """ Returns the unit vector of the vector.  """
+    return vector / np.linalg.norm(vector)
+
+def angle_between(v1, v2):
+    """ Returns the angle in radians between vectors 'v1' and 'v2'::
+
+            >>> angle_between((1, 0, 0), (0, 1, 0))
+            1.5707963267948966
+            >>> angle_between((1, 0, 0), (1, 0, 0))
+            0.0
+            >>> angle_between((1, 0, 0), (-1, 0, 0))
+            3.141592653589793
+    """
+    v1_u = unit_vector(v1)
+    v2_u = unit_vector(v2)
+    return np.arccos(np.clip(np.dot(v1_u, v2_u), -1.0, 1.0))
